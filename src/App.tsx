03250645d075afbe07dfaa52cea979c353eaf223
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2025-11-01 21:27:27 +0330

    needed routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-export default function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NoMatch from "./pages/NoMatch";
 
+export default function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* ProtectedRoute for more security */}
+          <Route path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } />
+          {/* NoMatch page for unavailable pages */}
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
